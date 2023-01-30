72e52860cc64ee67eab16e510ae84c05a50c1e9f
Anastasiia Malakhova <matviichuknastya@gmail.com>
2023-01-30 09:23:05 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}
+
+function currentTime() {
+  const time = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (time) {
+    player.setCurrentTime(time);
+  }
+}
+currentTime();
