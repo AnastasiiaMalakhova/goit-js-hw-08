9c9ad139d50069113522674905e0dc423cedf24f
Anastasiia Malakhova <matviichuknastya@gmail.com>
2023-01-30 14:34:38 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+const formData = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput({ target }) {
+  const key = target.name;
+  formData[key] = target.value;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function getStorageData() {
+  const data = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parseData = JSON.parse(data);
+  if (parseData) {
+    if (parseData.email) {
+      email.value = parseData.email;
+    }
+    if (parseData.message) {
+      message.value = parseData.message;
+    }
+  }
+}
+getStorageData();
